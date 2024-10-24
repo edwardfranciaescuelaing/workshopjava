39ea256faf55057f2dfd59f39f3bec7f7d578bf8
edwardfranciaescuelaing <edward.francia-g@mail.escuelaing.edu.co>
2024-10-24 18:56:27 -0500

    first commit

---
+++ src/main/java/com/mycompany/workshop/Workshop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.workshop;
+
+/**
+ *
+ * @author alexa
+ */
+public class Workshop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
